cf7fb8020835d2ffa4d4267ea85054887ac77b41
PedroM99 <pmsm1999@gmail.com>
2026-03-24 15:44:52 +0000

    Installed tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>MyFinance</h1>
+  return (
+    <div className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">MyFinance</h1>
+    </div>
+  );
 }
 
 export default App;
